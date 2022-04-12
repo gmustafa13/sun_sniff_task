ed1681a4c7b2a7dfd4099647531381a99f6e629c
gmustafa13 <gmustafa.mansoori@gmail.com>
2022-04-12 08:43:40 +0530

    test done

--- __test__/location.test.js
+++ __test__/location.test.js
@@ -23,4 +23,30 @@ describe('Testing location module', () => {
         expect(response.body.data[0].name).toEqual('Abbotsford')
 
     })
+
+    test('get location by lat and long along with 50 km nearby ',async()=>{
+        const response = await superTest(app)
+        .get(`${process.env.BASE_URL}/location`)
+        .query({ latitude:49.05798,longitude: -122.25257,radius:50 })
+    expect(response.status).toEqual(200)
+    expect(response.body.data.length).toEqual(17)
+    })
+
+    test('get location by wrong name', async() => {
+        const response = await superTest(app)
+            .get(`${process.env.BASE_URL}/location`)
+            .query({ q: "gulam_mustafa_mansuri",latitude:49.05798,longitude: -122.25257 })
+        expect(response.status).toEqual(200)
+        expect(response.body.data.length).toEqual(0)
+
+    })
+
+    test('get location by partial name', async() => {
+        const response = await superTest(app)
+            .get(`${process.env.BASE_URL}/location`)
+            .query({ q: "lon" })
+        expect(response.status).toEqual(200)
+        expect(response.body.data.length).toBeGreaterThan(0)
+
+    })
 })
